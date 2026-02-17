b129932f0df98446703c643065c93d64f8f06e94
Ali Imam Akash <akash1437aliimam@gmail.com>
2026-02-17 19:14:12 +0600

    handle contitional layouts

--- src/app/dashboard/layout.jsx
+++ src/app/dashboard/layout.jsx
@@ -1,20 +1,40 @@
-import Link from 'next/link';
-import React from 'react';
+import Link from "next/link";
+import React from "react";
 
-const DashboardLayout = ({children}) => {
-    return (
-        <div className='grid grid-cols-12 gap-5 min-h-screen'>
-            <div className='col-span-3 border-r-2'>
-                <h2 className='py-2 px-4 w-full bg-linear-to-r from-sky-50 to-sky-200 text-black rounded'><Link href={"/dashboard"}>Navigation</Link></h2>
-                <div className='flex flex-col gap-3 mt-10'>
-                    <Link className='py-2 px-4 w-full bg-gray-600 rounded' href={"/dashboard/add-story"}>Add Story</Link>
-                    <Link className='py-2 px-4 w-full bg-gray-600 rounded' href={"/dashboard/my-profile"}>My Profile</Link>
-                    <Link className='py-2 px-4 w-full bg-gray-600 rounded' href={"/dashboard/settings"}>Settings</Link>
-                </div>
-            </div>
-            <div className='col-span-9'>{children}</div>
+const DashboardLayout = ({ children }) => {
+  return (
+    <div className="grid grid-cols-12 gap-5 min-h-screen">
+      <div className="col-span-3 border-r-2">
+        <Link
+          href={"/"}
+          className="py-2 px-4 block w-full bg-linear-to-r from-sky-50 to-sky-200 text-black rounded"
+        >
+          ☯️ Dev-story
+        </Link>
+        <div className="flex flex-col gap-3 mt-10">
+          <Link
+            className="py-2 px-4 w-full bg-gray-600 rounded"
+            href={"/dashboard/add-story"}
+          >
+            Add Story
+          </Link>
+          <Link
+            className="py-2 px-4 w-full bg-gray-600 rounded"
+            href={"/dashboard/my-profile"}
+          >
+            My Profile
+          </Link>
+          <Link
+            className="py-2 px-4 w-full bg-gray-600 rounded"
+            href={"/dashboard/settings"}
+          >
+            Settings
+          </Link>
         </div>
-    );
+      </div>
+      <div className="col-span-9">{children}</div>
+    </div>
+  );
 };
 
 export default DashboardLayout;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,12 @@
+"use client"
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 const Header = () => {
+  const pathname = usePathname();
+  console.log(pathname)
+  if(pathname.startsWith("/dashboard")) return <></>
   return (
     <header className="flex text-center font-semibold text-2xl py-4 px-8 bg-green-600 justify-between items-center flex-wrap">
       <Link href="/">
