0dea29a2ecf48da07f7c4d388a1a2bd348165fb3
Ali Imam Akash <akash1437aliimam@gmail.com>
2026-02-17 15:53:24 +0600

    Create Multiple Layout in Next js

---
+++ src/app/dashboard/my-profile/page.jsx
@@ -0,0 +1,12 @@
+import Title from '@/components/Title';
+import React from 'react';
+
+const MyProfile = () => {
+    return (
+        <div>
+            <Title>My Profile Details</Title>
+        </div>
+    );
+};
+
+export default MyProfile;
